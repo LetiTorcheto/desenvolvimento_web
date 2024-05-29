a8f682aff2e98309a2824aa07c255ffb06546601
Leticia <leticia.torcheto@gmail.com>
2024-05-29 15:45:30 -0300

    banco de dados inicial de todas as pastas

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class alimento(models.Model):
+    localizacao = models.CharField(max_length=60)
+    data_validade = models.DateField()
+    tabela_nutricional = models.CharField(max_length=200)
+    tipo = models.CharField(max_length=20)
+
+class vestuario(models.Model):
+    localizacao = models.CharField(max_length=60)
+    cor = models.CharField(max_length=20)
+    tamanho = models.CharField(max_length=10)
+    tipo = models.CharField(max_length=20)
+    
+
+#usar para importar o banco os comandos:
+#pyton manage.py makemigrations
+#deposi pyton manage.py migrate para jogar para o banco de dados
+    
--- receptores/models.py
+++ receptores/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class receptor(models.Model):
+    cnpj = models.CharField(max_length=11)
+    nome = models.CharField(max_length=30)
+    
+
+#usar para importar o banco os comandos:
+#pyton manage.py makemigrations
+#deposi pyton manage.py migrate para jogar para o banco de dados
+    
