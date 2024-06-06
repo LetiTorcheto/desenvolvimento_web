584767beea4166682ef052026ef427da070d8846
Leticia <leticia.torcheto@gmail.com>
2024-06-06 17:16:09 -0300

    alteração em templates no produtos

--- produtos/views.py
+++ produtos/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def produtos(request):
-    return HttpResponse('Produtos')
+    return render(request, 'produtos.html')
 
 def alimentos(request):
     return HttpResponse('Alimentos')
