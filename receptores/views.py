c296228cb01918f8ace3f85ba995ab53e13e3f52
Leticia <leticia.torcheto@gmail.com>
2024-06-09 14:37:41 -0300

    tela de login receptor feita

--- receptores/views.py
+++ receptores/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def receptor(request):
-    return render(request, 'receptor.html')
+    return render(request, 'login.html')
